2e9b6d590351614c2456ba3474fe2e5f611a4b70
TL <TL@gmail.com>
2024-04-01 21:24:35 +0530

    Utility java Modified

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -8,6 +8,10 @@ public class Utility {
 		return new Date();
 	}
 	
+	public void m1() {
+		System.out.println("Utility m1() Method");
+	}
+	
 public static void main(String[] args) {
 	
 	System.out.println();
