f21c8075fcef00a3f185860f6b756ae36847fc65
TL <TL@gmail.com>
2024-04-02 16:55:03 +0530

    T1 Y1 Method

---
+++ DemoProj1/src/com/test/Y1.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Y1 {
+	
+	public void Y1() {
+		System.out.println("This is Y method");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
